e985cf15219714a111c80bf13c1927638d33a771
pmattos <pmattos@constanciainvest.com>
2016-03-31 17:04:56 -0300

    Implemented sequence analysis for company-only

--- script.R
+++ script.R
@@ -22,6 +22,20 @@ getSequence <- function(raw){
 
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
+makeSequencePlots <- function(seq){
+  seqIplot(seq,sortv="from.start",withlegend=TRUE,title="Companies - All sequences - Sort by start")
+  
+  seqIplot(seq,sortv="from.end",withlegend=TRUE,title="Companies - All sequences - Sort by end")
+  
+  seqfplot(seq, withlegend = TRUE, border = NA, title="10 Most Frequent Sequences")
+  
+  seqdplot(seq, withlegend = TRUE, border = NA, title="States Distribution Over Time")
+  
+  seqmtplot(seq, withlegend = TRUE, title="Mean Time spent in each State")
+  return(TRUE)
+}
+
+
 
 # I - COMPANIES ANALYSIS
 companiesSequence <- getSequence(companies)
@@ -47,37 +61,34 @@ for (row in seq(1:dim(companiesSequence)[1])){
 companiesSequence.scodes <- unique(unlist(companiesSequence))
 companiesSequence.alphabet <- unique(unlist(companiesSequence))
 companiesSequence.labels <- unique(unlist(companiesSequence))
-# companiesSequence.seq <- seqdef(companiesSequence,NULL,states=companiesSequence.scodes,labels=companiesSequence.labels, alphabet=companiesSequence.alphabet,missing="Z")
+companiesSequence.seq <- seqdef(companiesSequence,NULL,states=companiesSequence.scodes,labels=companiesSequence.labels, alphabet=companiesSequence.alphabet,missing="Z")
+
+
 
 # Vanilla sequence
 companiesSequence.seq <- seqdef(companiesSequence,NULL,missing="Z")
 
 cpal(companiesSequence.seq) <- brewer.pal(3,"Spectral")
 
-seqIplot(companiesSequence.seq,sortv="from.start",withlegend=TRUE,title="Companies - All sequences - Sort by start")
+makeSequencePlots(companiesSequence.seq)
 
-seqIplot(companiesSequence.seq,sortv="from.end",withlegend=TRUE,title="Companies - All sequences - Sort by end")
-
-seqfplot(companiesSequence.seq, withlegend = TRUE, border = NA, title="10 Most Frequent Sequences")
-
-seqdplot(companiesSequence.seq, withlegend = TRUE, border = NA, title="States Distribution Over Time")
-
-seqmtplot(companiesSequence.seq, withlegend = TRUE, title="Mean Time spent in each State")
 
 # Synchronized Sequence -  Deleting values to the left and to the right 
-companiesSequence.seq <- seqdef(companiesSequence,NULL,missing="Z",right="DEL",left="DEL")
+companiesSequence.scodes <- c('A','B','C')
+companiesSequence.alphabet <- c('A','B','C')
+companiesSequence.labels <- c('A','B','C')
+companiesSequence.seq <- seqdef(companiesSequence,NULL,states=companiesSequence.scodes,labels=companiesSequence.labels, alphabet=companiesSequence.alphabet,missing="Z",,right="DEL",left="DEL",void="Z")
 
-cpal(companiesSequence.seq) <- brewer.pal(3,"Spectral")
+companiesSequence.seq <- companiesSequence.seq[-c(24,44,178,229,330),]
 
-seqIplot(companiesSequence.seq,sortv="from.start",withlegend=TRUE,title="Companies - All sequences - Sort by start")
+companiesSequence.seq <- companiesSequence.seq[-226,]
 
-seqIplot(companiesSequence.seq,sortv="from.end",withlegend=TRUE,title="Companies - All sequences - Sort by end")
-
-seqfplot(companiesSequence.seq, withlegend = TRUE, border = NA, title="10 Most Frequent Sequences")
-
-seqdplot(companiesSequence.seq, withlegend = TRUE, border = NA, title="States Distribution Over Time")
-
-seqmtplot(companiesSequence.seq, withlegend = TRUE, title="Mean Time spent in each State")
+# dim(companiesSequence.seq)
+# 
+# sum(rowSums(companiesSequence.seq == "Z") == 71)
+# x<-which(rowSums(companiesSequence.seq == "*") == 71)
+# 
+# companiesSequence.seq[226,]
 
 
 
@@ -99,14 +110,12 @@ seqIplot(companiesSequence.seq,sortv="from.start",group=clusters,withlegend=TRUE
 
 
 
+# III. Event
+companiesSequence.seqe <- seqecreate(companiesSequence.seq,tevent="state", use.labels=FALSE)
+subsSequences <- seqefsub(companiesSequence.seqe,strsubseq=c("(A)-(C)-(A)","(A)-(B)-(A)"))
+print(subsSequences)
+subsSequences.pres <- seqeapplysub(subsSequences[,], method="presence")
+index <- which(subsSequences.pres[,1]==1)
 
-
-
-dim(companies)
-dim(getSequence(companies))
-
-unlist(getSequence(companies))
-
-unique(unlist(getSequence(companiesSequence)))
-
+# Parei no DistanceUnitaryCmns - LOOKING FOR FREQUENTE SUBSEQUENCES
 
